6095f06b3b2487957cf54346b77cd1ebbf7c1ce8
Ismael Correa <ismael.correa.25@gmail.com>
2022-03-31 11:48:20 -0300

    code done

--- proxy_http.py
+++ proxy_http.py
@@ -29,7 +29,6 @@ if __name__ == "__main__":
 
       # Guardamos el host
       host = received_request.headers["Host"]
-      # print(host)
 
       try:
         # Si recibimos un archivo json por consola leemos sus contenido
@@ -40,9 +39,12 @@ if __name__ == "__main__":
 
       except:
         print("Error when trying to read config file")
+        break
 
       else:
 
+        # Si la dirección del recurso se encuentra en la lista de direcciones
+        # bloqueadas, luego respondemos al cliente con un error 403
         if received_request.start_line['resource'] in config['blocked']:
           
           body = "<h1>Error 403, Blocked address</h1>"
@@ -52,12 +54,16 @@ if __name__ == "__main__":
             start_line={'http_version':received_request.start_line['http_version'],
                         'status_code':'403',
                         'status_text':'Blocked address'},
-            headers={'Content-Length':str(len(body.encode()))},
+            headers={'Content-Length':str(len(body.encode())),
+                     'Content-Type': 'text/html'},
             body=body
           ))
           response = response.encode()
           client_connection.send(response)
         
+        # De lo contrario agregamos el correo como header y redirigimos el request
+        # al servidor, finalmente redactando la respuesta recibida de manera concordante
+        # con el las palabras prohibidas y sus reemplazos, y redirigiendo al cliente.
         else:
 
           # Agregamos nuestro correo a los headers del request
@@ -77,15 +83,13 @@ if __name__ == "__main__":
 
           received_response = receive_and_parse_http_message(server_socket, buff_size, 'response')
 
+          # Redactamos el response de acuerdo a las palabras prohibidas.
           received_response.redact(config['forbidden_words'])
 
           received_response = create_http_msg(received_response)
           received_response = (received_response).encode()
           client_connection.send(received_response)
-          # cerramos la conexión
-          # notar que la dirección que se imprime indica un número de puerto distinto al 8888
 
-          # seguimos esperando por si llegan otras conexiones
 
           server_socket.close()
 
